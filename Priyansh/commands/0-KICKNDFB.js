0eb02a3866c92bef975b337ed12d7e3504571a47
Rowedy413 <rinkudadar1234@gmail.com>
2025-07-15 10:49:06 +0530

    Update 0-KICKNDFB.js

--- Priyansh/commands/0-KICKNDFB.js
+++ Priyansh/commands/0-KICKNDFB.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "kickndfb",
     version: "1.0.0",
     hasPermssion: 1,
-    credits: "SHANKAR",
+    credits: "𝗥𝟬𝗪𝟯𝗗𝗬 𝗞𝗜𝗜𝗡𝗚",
     description: "Filter Facebook users",
     commandCategory: "Group",
     usages: "",
